66760e176d1e8e8558cb2d5e6b4df263b08e9fb8
Andy Nenthong <C0516154@intra.camosun.bc.ca>
2021-11-15 12:06:54 -0800

    Add project files.

---
+++ RandomValuesApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RandomValuesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
