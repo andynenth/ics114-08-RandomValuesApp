fd16df9b58f262e827bc0ae349c9c703855c1527
Andy Nenthong <C0516154@intra.camosun.bc.ca>
2021-11-15 12:19:20 -0800

    in lab

--- RandomValuesApp/Program.cs
+++ RandomValuesApp/Program.cs
@@ -6,7 +6,27 @@ namespace RandomValuesApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Row: ");
+            int row = int.Parse(Console.ReadLine());
+            Console.WriteLine("Col: ");
+            int col = int.Parse(Console.ReadLine());
+            int[,] numbers = new int[row, col];
+
+            Console.WriteLine("Random Generated Values\n");
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    //Gernerate a random number
+                    
+
+
+                    if (j==0)
+                    {
+                        Console.WriteLine(String.Format("Row {0,-8}", i));
+                    }
+                }
+            }
         }
     }
 }
