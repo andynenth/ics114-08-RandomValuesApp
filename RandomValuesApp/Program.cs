617ee836a974948b29ffd3b03bcb7f72166fca3b
andynenth <andy.nenth@gmail.com>
2021-11-18 23:04:01 -0800

    Add GetValidNumber

--- RandomValuesApp/Program.cs
+++ RandomValuesApp/Program.cs
@@ -12,32 +12,38 @@ namespace RandomValuesApp
             int[] position = new int[2];
             
             Console.Write("Row: ");
-            int totalRow = GetValue();
+            int totalRow = GetValidNumber(1,20);
             Console.Write("Col: ");
-            int totalCol = GetValue();
+            int totalCol = GetValidNumber(1,20);
             int[,] numbers = new int[totalRow, totalCol];
             
             Console.WriteLine("Random Generated Values\n");
             
             Console.Write("{0,-10}","Col");
-
+            
+            //Print column numbers.
             for (int col = 0; col < totalCol; col++)
             {
                 Console.Write("{0, 6}",col+1);
             }
             Console.WriteLine();
+            
             for (int row = 0; row < numbers.GetLength(0); row++)
             {
+                //Print row numbers.
                 Console.Write("Row {0, -6}", row+1);
-                for (int j = 0; j < numbers.GetLength(1); j++)
+                for (int col = 0; col < numbers.GetLength(1); col++)
                 {
-                    //Gernerate a random number
-                    numbers[row, j] = random.Next(1,101);
-                    Console.Write("{0, 6}", numbers[row, j]);
-                    if (numbers[row, j] > maxValue)
+                    //Generate a random number from 1 to 100.
+                    numbers[row, col] = random.Next(1,101);
+                    //Print cell's value.
+                    Console.Write("{0, 6}", numbers[row, col]);
+                    if (numbers[row, col] > maxValue)
                     {
-                        maxValue = numbers[row, j];
-                        position = new int[] {row, j};
+                        //Record new maximum value.
+                        maxValue = numbers[row, col];
+                        //Record new position.
+                        position = new int[] {row, col};
                     }
                 }
                 Console.WriteLine();
@@ -47,18 +53,16 @@ namespace RandomValuesApp
             Console.WriteLine("Col "+ (position[1]+1));
         }
 
-        public static int GetValue()
+        public static int GetValidNumber(int min, int max)
         {
-            string inval = Console.ReadLine();
-            int number;
-            if (int.TryParse(inval, out number) && number > 0 && number < 21)
+            if (int.TryParse(Console.ReadLine(), out int result) && result >= min && result <= max)
             {
-                return number;
+                return result;
             }
             else
             {
-                Console.WriteLine("Please enter a number between 1 to 20.");
-                return GetValue();
+                Console.WriteLine("Please enter a number between {0} to {1}.", min, max);
+                return GetValidNumber(min, max);
             }
         }
     }
